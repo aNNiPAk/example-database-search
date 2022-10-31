fc56810d2c628a91e3ff9776b7f0b5cea147c167
aNNiPAk <annipak@yandex.ru>
2022-10-31 17:21:57 +0300

    feat: added functionality for the FindTextInDB function

--- app/db_search.js
+++ app/db_search.js
@@ -1,8 +1,24 @@
 const {db} = require('./db')
 
 const FindTextInDB = async (tableName, searchText) => {
-    const result = await db.query(`SELECT * FROM table1`, [])
-    console.log(result)
+    const query = `SELECT * FROM ${tableName} 
+        WHERE lower(name) like '%${searchText.toLowerCase()}%' or lower(description) like '%${searchText.toLowerCase()}%'
+        ORDER BY name, description`
+
+    const queryResult = await db.query(query)
+    
+    if (queryResult.rowCount > 20) {
+        return {
+            data: queryResult.rows.slice(0, 20),
+            count: queryResult.rowCount
+        }
+    } else {
+        return {
+            data: queryResult.rows,
+            count: queryResult.rowCount
+        }
+    }
+
 }
 
-module.exports = {FindTextInDB}
+module.exports = FindTextInDB
--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
-const {FindTextInDB} = require('./db_search')
+const FindTextInDB = require('./db_search')
 
+const main = async () => {
+    const result = await FindTextInDB("table1", "a")
+    console.log(result)
+}
 
-const result = FindTextInDB("table1", "and")
-console.log(result)
+main()
